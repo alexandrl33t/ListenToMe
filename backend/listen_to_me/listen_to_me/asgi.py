a0c8a529618b69fb8c6f76a1ff01a31b7a861738
Alexandr <alexandrl33t@yandex.ru>
2023-09-25 22:10:36 +0300

    first commit

---
+++ backend/listen_to_me/listen_to_me/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for listen_to_me project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'listen_to_me.settings')
+
+application = get_asgi_application()
